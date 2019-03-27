831e11cc969d1faa0899189840ab6a0e6857367d
nic33wild <Nicola-Trentini_student2018@wilder.school>
2019-03-27 19:39:34 +0100

    new Tabs component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,59 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
 
-import Fetch from "./components/Fetch";
 import UserPanel from "./components/UserPanel";
 import TabBarNew from "./components/TabBarNew";
 import FriendsTab from "./components/FriendsTab";
 import UpcomingEventsTab from "./components/UpcomingEventsTab";
+import TabsComponent from "./components/TabsComponent";
+import Tabs from "./components/Tabs";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { active: "UpcomingEventsTab" };
+  }
+
   render() {
+    const content = {
+      UpcomingEventsTab: <div><UpcomingEventsTab/></div>,
+      FriendsTab: <div><FriendsTab/></div>,
+    };
+
     return (
       <React.Fragment>
-       <Router>
+        <Router>
           <div>
             <slot name="profilepage">
               <div className="container">
                 <div className="row">
-                  <div className="col-3">
-                    <UserPanel>
-                      <Fetch />
-                    </UserPanel>
+                  <div className="col-lg-3 col-xs-12">
+                    <UserPanel />
                   </div>
-                  <div className="col-9">
+                  <div className="col-lg-9 col-xs-12">
+                    <Tabs
+                      active={this.state.active}
+                      onChange={active => this.setState({ active })}
+                    >
+                      <div key="UpcomingEventsTab">Upcoming Events</div>
+                      <div key="FriendsTab">Friends</div>
+                    </Tabs>
                     <div>
-                      <UpcomingEventsTab />
-                    </div>
-
-                    <div>
-                      <FriendsTab />
+                      {content[this.state.active]}
                     </div>
                   </div>
                 </div>
               </div>
             </slot>
           </div>
-          </Router>
+        </Router>
       </React.Fragment>
     );
   }
