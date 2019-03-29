79fbb341cde85da4280934e0c09e58c85172184b
nic33wild <Nicola-Trentini_student2018@wilder.school>
2019-03-29 15:58:04 +0100

    css react-tabs

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Link } from "react-router-dom";
 
-import UserPanel from "./components/UserPanel";
-import FriendsTab from "./components/FriendsTab";
-import UpcomingEventsTab from "./components/UpcomingEventsTab";
 import Router from "./Router";
-import TabBarNew from "./components/TabBarNew";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { active: "UpcomingEventsTab" };
-  }
 
   render() {
-    const content = {
-      UpcomingEventsTab: (
-        <div>
-          <UpcomingEventsTab />
-        </div>
-      ),
-      FriendsTab: (
-        <div>
-          <FriendsTab />
-        </div>
-      )
-    };
 
     return (
       <React.Fragment>
--- src/components/TabBarNew.js
+++ src/components/TabBarNew.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import "react-tabs/style/react-tabs.css";
+import { Tab, Tabs, TabList } from "react-tabs";
+import "./Tabs.css";
 
 const PATHS = "/players/";
 const PATHEE = "/upcoming";
@@ -11,12 +11,16 @@ class TabBarNew extends Component {
   render() {
     return (
       <div className="col-lg-9 col-xs-12 offset-lg-3">
-      <Tabs>
-        <TabList>
+        <Tabs>
+        <TabList activeTabClassName='is-selected'>
+          <Tab>
           <Link to={PATHS + this.props.match.params.id + PATHEE}>
             Upcoming Events
           </Link>
+        </Tab>
+        <Tab>
           <Link to={PATHS + this.props.match.params.id + PATHEF}>Friends</Link>
+          </Tab>
         </TabList>
       </Tabs>
       </div>
