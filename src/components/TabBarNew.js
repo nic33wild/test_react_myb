f511cd3b9a69c24c0eeb04ef86f7c2d8c223fdc9
nic33wild <Nicola-Trentini_student2018@wilder.school>
2019-03-30 02:24:01 +0100

    some css

--- src/components/TabBarNew.js
+++ src/components/TabBarNew.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-
+import { Tab, Tabs, TabList } from "react-tabs";
 
 import "./Tabs.css";
 
@@ -14,7 +12,7 @@ class TabBarNew extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeTab: 2
+      activeTab: 0
     };
     this.handleClickActiveTab = this.handleClickActiveTab.bind(this);
   }
@@ -25,41 +23,74 @@ class TabBarNew extends Component {
 
 
   render() {
-    const activeTab = 1;
+    const activeTab = 0;
     return (
       <div className="col-lg-9 col-xs-12 offset-lg-3">
-        <div className="container">
-
+        
           <Tabs>
-            <TabList>
-              <Tab
-                className={activeTab === 1 ? "react-tabs__tab--selected" : ""}
+
+              <TabList
+                className={activeTab === 0 ? "react-tabs__tab--selected" : "react-tabs__tab"}
               >
-                <Link
+              <Tab>
+                <Link style={{ textDecoration: 'none' }}
                   to={PATHS + this.props.match.params.id + PATHEE}
-                  onClick={this.handleClickActiveTab.bind(this, 1)}
-                  styleName="nav-tabs__item-link"
+                  onClick={this.handleClickActiveTab.bind(this, 0)}
                 >
                   Upcoming Events
                 </Link>
-              </Tab>
+                </Tab>
               <Tab
-              className={activeTab === 2 ? "Tabs__Tab--active" : "Tabs__Tab"}
+              className={activeTab === 1 ? "react-tabs__tab--selected" : "react-tabs__tab"}
               >
-                <Link
+                <Link style={{ textDecoration: 'none' }}
                   to={PATHS + this.props.match.params.id + PATHEF}
-                  onClick={this.handleClickActiveTab.bind(this, 2)}
-                  styleName="nav-tabs__item-link"
+                  onClick={this.handleClickActiveTab.bind(this, 1)}
                 >
                   Friends
                 </Link>
-              </Tab>
-            </TabList>
+                </Tab>
+
+              </TabList>
+
+              
           </Tabs>
         </div>
-      </div>
     );
   }
 }
 
 export default TabBarNew;
+
+
+// import React, { Component } from "react";
+// import { Link } from "react-router-dom";
+// import { Tab, Tabs, TabList } from "react-tabs";
+// import "./Tabs.css";
+
+// const PATHS = "/players/";
+// const PATHEE = "/upcoming";
+// const PATHEF = "/friends";
+
+// class TabBarNew extends Component {
+//   render() {
+//     return (
+//       <div className="col-lg-9 col-xs-12 offset-lg-3">
+//         <Tabs>
+//         <TabList activeTabClassName='is-selected'>
+//           <Tab>
+//           <Link to={PATHS + this.props.match.params.id + PATHEE}>
+//             Upcoming Events
+//           </Link>
+//         </Tab>
+//         <Tab>
+//           <Link to={PATHS + this.props.match.params.id + PATHEF}>Friends</Link>
+//           </Tab>
+//         </TabList>
+//       </Tabs>
+//       </div>
+//     );
+//   }
+// }
+
+// export default TabBarNew;
